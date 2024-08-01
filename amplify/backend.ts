4a792d215d69e2755f5b4c41490500171c645cb6
wagadao.y <wagadao.y@gmail.com>
2024-08-01 23:11:35 +0900

    iot rule

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,8 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { sayHello } from './functions/say-hello/resource';
-import * as appsync from 'aws-cdk-lib/aws-appsync';
+import * as iot from 'aws-cdk-lib/aws-iot'
+import * as iam from 'aws-cdk-lib/aws-iam'
 
 const backend = defineBackend({
   auth,
@@ -10,31 +11,29 @@ const backend = defineBackend({
   sayHello
 });
 
+
+const customResourceStack = backend.createStack('CustomResourceStack');
+
+const topic_rule = new iot.CfnTopicRule(customResourceStack, "MyTopicRule", {
+  topicRulePayload: {
+    sql: "SELECT * FROM 'iot/#'",
+    actions: [
+      {
+        lambda: {
+          functionArn: backend.sayHello.resources.lambda.functionArn
+        }
+      }
+    ]
+  }
+});
+
+// const iamArnPrincipal = new iam.ArnPrincipal(topic_rule.attrArn);
+const iotServicePrincipal = new iam.ServicePrincipal('iot.amazonaws.com');
+backend.sayHello.resources.lambda.grantInvoke(iotServicePrincipal);
+
+// amplify_outputs.jsonにLambda関数名を出力する
 backend.addOutput({
   custom: {
     sayHelloFunctionName: backend.sayHello.resources.lambda.functionName,
   },
 });
-
-const authenticatedUserIamRole = backend.auth.resources.authenticatedUserIamRole;
-backend.sayHello.resources.lambda.grantInvoke(authenticatedUserIamRole);
-
-backend.data.resources.graphqlApi.grant(
-  backend.sayHello.resources.lambda,
-  appsync.IamResource.all(),
-  'appsync:GraphQL'
-);
-
-// // Lambda に AppSync GraphQL API を実行する権限を与える
-// backend.data.resources.graphqlApi.grant(
-//   backend.sayHello.resources.lambda,
-//   cdk.aws_appsync.IamResource.all(),
-//   'appsync:GraphQL'
-// );
-
-// // Lambda 関数の環境変数に AppSync のエンドポイント URL を設定
-// backend.sayHello.resources.cfnResources.cfnFunction.environment = {
-//   variables: {
-//     ENDPOINT: backend.data.resources.cfnResources.cfnGraphqlApi.attrGraphQlUrl,
-//   }
-// };
--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -1,6 +1,17 @@
+import { Amplify } from 'aws-amplify';
 import type { Handler } from 'aws-lambda';
+import { generateClient } from "aws-amplify/data";
+
+import type { Schema } from "../../data/resource";
+import outputs from "../../../amplify_outputs.json";
+
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
 
 export const handler: Handler = async (event, context) => {
-  return { message: 'Hello, World!!' };
+  await client.models.Todo.create({ content: event.message });
+  console.log(event);
+  // return { message: 'Hello, World!!' };
 };
 
