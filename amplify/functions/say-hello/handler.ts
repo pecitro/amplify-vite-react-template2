b7a0e8926a3e2dccc103987b728885c6a7cc0c62
wagadao.y <wagadao.y@gmail.com>
2024-08-05 21:55:17 +0900

    LambdaにAppSync経由での書き込みを追加

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -3,15 +3,52 @@ import type { Handler } from 'aws-lambda';
 import { generateClient } from "aws-amplify/data";
 
 import type { Schema } from "../../data/resource";
-import outputs from "../../../amplify_outputs.json";
+import { env } from '$amplify/env/say-hello';
+import { createTodo } from './mutations';
 
-Amplify.configure(outputs);
+// スキーマを書き換えたら、以下のコマンドを実行してGraphQLクライアントを再生成する
+// npx ampx generate graphql-client-code
+
+Amplify.configure(
+  {
+    API: {
+      GraphQL: {
+        endpoint: env.AMPLIFY_DATA_GRAPHQL_ENDPOINT,
+        region: env.AWS_REGION,
+        defaultAuthMode: 'identityPool',
+      }
+    }
+  },
+  {
+    Auth: {
+      credentialsProvider: {
+        getCredentialsAndIdentityId: async () => ({
+          credentials: {
+            accessKeyId: env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+            sessionToken: env.AWS_SESSION_TOKEN
+          }
+        }),
+        clearCredentialsAndIdentityId: () => { }
+      }
+    }
+  }
+);
+
 
 const client = generateClient<Schema>();
 
 export const handler: Handler = async (event, context) => {
-  await client.models.Todo.create({ content: event.message });
   console.log(event);
-  // return { message: 'Hello, World!!' };
+  console.log(client);
+
+  await client.graphql({
+    query: createTodo,
+    variables: {
+      input: {
+        content: event.message
+      }
+    }
+  });
 };
 
